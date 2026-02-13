55a660299fb262c6ea5327de084c15b4ee49a5b5
Ade <93434339+Fruitpunch44@users.noreply.github.com>
2026-02-13 01:52:07 +0100

    use async method first change to sync method later

--- notification.c
+++ notification.c
@@ -6,6 +6,23 @@
 #define WM_TRAYICON (WM_USER + 1)
 
 //create a window
+LRESULT CALLBACK KeyboardProc(int nCode,WPARAM wParam,LPARAM lParam){
+    if(nCode == HC_ACTION){
+        KBDLLHOOKSTRUCT *kbp = (KBDLLHOOKSTRUCT*) lParam;
+        char buff[40];
+        switch(wParam){
+            case WM_KEYDOWN:   
+                if(kbp->vkCode == 'c'){
+                    PostQuitMessage(0);
+                    exit(0);
+        }
+        default:
+        return CallNextHookEx(NULL,nCode,wParam,lParam);
+    }
+
+    }
+    return CallNextHookEx(NULL,nCode,wParam,lParam);
+}
 
 LRESULT CALLBACK Wndproc(HWND hwnd,UINT message,WPARAM wparam,LPARAM lparam){
     switch(message){
@@ -47,17 +64,21 @@ hwnd =CreateWindowEx(
     nid.uCallbackMessage = WM_TRAYICON;
     nid.hIcon=LoadIcon(NULL,IDI_INFORMATION);
     nid.dwInfoFlags = NIIF_INFO;
+    lstrcpy(nid.szTip,"THIS DOES NOT DO ANYTHING");
     lstrcpy(nid.szInfoTitle,"test notification");
-    lstrcpy(nid.szInfo,"this is a test");
+    lstrcpy(nid.szInfo,"this is a test i do not how to advance");
     Shell_NotifyIcon(NIM_MODIFY,&nid);
     Shell_NotifyIcon(NIM_ADD,&nid);
+
+    HINSTANCE instance = GetModuleHandle(NULL);
+    HHOOK keys= SetWindowsHookEx(WH_KEYBOARD_LL,KeyboardProc,instance,0);
 
     MSG msg;
     while(GetMessage(&msg,NULL,0,0)){
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
-
+    UnhookWindowsHookEx(keys);
     Shell_NotifyIcon(NIM_DELETE,&nid);
     return 0;
 
